5db7bda0c2bbaa195ccb7321e52edfa07ba098ae
sovietbot2020 <65543251+sovietbot2020@users.noreply.github.com>
2020-05-18 21:00:30 +0900

    Update sovietbot.py

--- sovietbot.py
+++ sovietbot.py
@@ -3,11 +3,12 @@ import asyncio
 from discord.ext import commands
 import random
 from discord.utils import get
+import os
 
 
 client = commands.Bot(command_prefix='크리스')
 
-token = "NzExNjM0ODM5NjA1ODcwNjkz.XsF3uQ.aUyXdRos9gszxQ6uknynt2c6F_8"
+token = "access_token"
 
 @client.event
 async def on_ready():
@@ -93,5 +94,5 @@ async def _unmute(ctx, member: discord.Member=None):
 
 
 
-
+access_token = os.environ["BOT_TOKEN"]
 client.run(token)
